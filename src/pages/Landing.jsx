9a883f5eb84fcedf291e5e04264d08437d9a519e
Andrew Chen <andrew831119work@gmail.com>
2023-09-18 13:44:45 +0800

    first commit

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,18 +4,18 @@ import { NavLink } from "react-router-dom";
 
 const Landing = () => {
   return (
-    <main className="min-h-screen">
-      <nav className="align-element py-6 flex space-x-4 items-center">
+    <main className="min-h-screen align-element">
+      <nav className="py-6 flex justify-center lg:justify-start space-x-4 items-center">
         <Logo imgSize="h-12 w-12" textSize="4xl" />
       </nav>
       <div className="pt-12">
-        <div className="align-element grid items-center lg:grid-cols-2 lg:gap-x-8">
+        <div className="grid text-center items-center lg:text-left lg:grid-cols-2 lg:gap-x-8">
           {/* info */}
           <div>
             <h1 className="text-4xl tracking-wide font-bold leading-loose lg:text-5xl">
               Job <span className="text-secondary">Tracking</span> App
             </h1>
-            <p className="mt-4 leading-7 max-w-[35rem] text-gray-500 lg:mt-6">
+            <p className="mt-4 mx-auto lg:mx-0 leading-7 max-w-[35rem] text-gray-500 lg:mt-6">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
               quas corrupti quaerat obcaecati necessitatibus dolorem perferendis
               consequuntur quibusdam iusto tempore.
